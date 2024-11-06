abb4497e59413f0818afb3c0b749bd9784044d13
Hashan Kavinda <delpierokavinda@gmail.com>
2024-11-06 20:45:27 +1000

    feat: added model for the calculator

---
+++ Calculator/Models/CalculatorModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public double NumberOne { get; set; }
+        public double NumberTwo { get; set; }
+        public double Operator { get; set; }
+        public double Answer { get; set; }
+    }
+}
