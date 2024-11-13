67a65adf0333b86c7e2c583bb8a132468bef7a7c
Hashan Kavinda <delpierokavinda@gmail.com>
2024-11-13 12:49:45 +1000

    fix: remove unnecessary code blocks

--- Calculator/Controllers/CalculatorController.cs
+++ Calculator/Controllers/CalculatorController.cs
@@ -1,33 +1,20 @@
 ﻿using Calculator.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Caching.Memory;
+using System.Data;
 
 namespace Calculator.Controllers
 {
     public class CalculatorController : Controller
     {
-        private readonly ILogger<CalculatorController> _logger;
-        private readonly IMemoryCache _cache;
-        private readonly string cacheNumberOne = "numberOne";
-        private readonly string cacheNumberTwo = "numberTwo";
-        private readonly string cacheOperator = "operator";
-        private readonly string cacheKey = "cacheKey";
-
-        public CalculatorController(ILogger<CalculatorController> logger, IMemoryCache cache)
-        {
-            _logger = logger;
-            _cache = cache;
-        }
-
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View(new CalculatorModel());
         }
 
         [HttpPost]
-        public IActionResult Index(CalculatorModel cm, string btnName)
+        public ActionResult Index(CalculatorModel cm)
         {
-            cm.Placeholder = "hello";
+            cm.Placeholder = Convert.ToString(new DataTable().Compute(cm.Placeholder, null)) ?? "0";
             return View(cm);
         }
     }
